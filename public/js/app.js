4e1b544f838ac6c2726c4b537f358e6948846cd5
bradleymarch <bradleymarch@gmail.com>
2017-05-21 11:31:55 -0500

    mock-data-draft

---
+++ public/js/app.js
@@ -0,0 +1,138 @@
+$(function() {
+
+  
+
+const MOCK_FORECAST = [
+  {
+    day: 'Today',
+    forecast: [
+      {
+        time: 'Morning',
+        temp: 75,
+        condition: 'Sunny',
+        clothes: {
+
+          upperBody: 'short',
+          lowerBody: 'short',
+          jacket: 'none',
+        }
+      },
+      {
+        time: 'Afternoon',
+        temp: 79,
+        condition: 'Sunny',
+        clothes: {
+
+          upperBody: 'short',
+          lowerBody: 'short',
+          jacket: 'none',
+        }
+      },
+      {
+        time: 'Evening',
+        temp: 68,
+        condition: 'Partly Cloudy',
+        clothes: {
+
+          upperBody: 'short',
+          lowerBody: 'long',
+          jacket: 'none',
+        }      
+      },
+    ],
+  },
+
+  {
+
+    day: 'Tomorrow',
+    forecast: [
+      {
+        time: "Morning",
+        temp: 56,
+        condition: 'Cloudy',
+        clothes: {
+
+          upperBody: 'long',
+          lowerBody: 'long',
+          jacket: 'light',
+        }
+      },
+
+      {
+        time: 'Afternoon',
+        temp: 68,
+        condition: 'Sunny',
+        clothes: {
+
+          upperBody: 'short',
+          lowerBody: 'long',
+          jacket: "none",
+        }
+      },
+
+      {
+        time: 'Evening',
+        temp: 70,
+        condition: 'Sunny',
+        clothes: {
+
+          upperBody: 'short',
+          lowerBody: 'long',
+          jacket: "none",
+        }
+      }
+
+
+
+    ]
+  }
+
+
+];
+
+
+// this happens after enter a location to SET
+  function getForecast(callbackFn) {
+      setTimeout(function(){ callbackFn(MOCK_FORECAST)}, 100);
+  }
+
+  // this function stays the same when we connect
+  // to real API later
+  function displayForecast(data) {
+      for (const item of data) {
+         $('body').append(
+          '<p>' + item.time + '</p>');
+         console.log(item);
+      }
+  }
+
+  // this function can stay the same even when we
+  // are connecting to real API
+  function getAndDisplayForecast() {
+      getForecast(displayForecast);
+  }
+
+
+
+  $(function(addEventListeners) {
+
+    $(".js-sign-in-button").on("submit", function(event) {
+
+      $(".js-sign-in-view").addClass("hidden");  
+      $(".js-profile-view").removeClass("hidden"); 
+    });
+
+    $(".js-location-button").on("click", function() {
+      getAndDisplayForecast();
+
+    });
+    $(function() {
+      addEventListeners();
+    });
+
+  });
+
+
+
+
+});
--- server.js
+++ server.js
@@ -3,4 +3,26 @@ var app = express();
 app.use(express.static('public'));
 app.listen(process.env.PORT || 8080);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = { app };
