b1fbad03556937214a30195a167ea3a37a34c4ad
bradleymarch <bradleymarch@gmail.com>
2017-06-29 18:14:17 -0500

    tests

--- test/server.test.js
+++ test/server.test.js
@@ -1,20 +1,15 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const {TEST_DATABASE_URL} = require('../config');
 
-const {app} = require('/server');
-const {TEST_DATABASE_URL} = require('/config');
+const {app, runServer, closeServer} = require('../server');
 
-// this lets us use *should* style syntax in our tests
-// so we can do things like `(1 + 1).should.equal(2);`
-// http://chaijs.com/api/bdd/
 const should = chai.should();
 
-// This let's us make HTTP requests
-// in our tests.
-// see: https://github.com/chaijs/chai-http
 chai.use(chaiHttp);
 
-describe('first-test', function() {
+
+describe('root test', function() {
   before(function() {
     return runServer(TEST_DATABASE_URL);
   });
--- test/test-users-itegration-tests.js
+++ test/test-users-itegration-tests.js
@@ -4,9 +4,9 @@ const mongoose = require('mongoose');
 const faker = require('faker');
 const bcrypt = require('bcryptjs');
 const should = chai.should();
-//const {User} = require('./models');
-const {app, runServer, closeServer} = require('/server');
-const {TEST_DATABASE_URL} = require('/config');
+const {User} = require('../models');
+const {app, runServer, closeServer} = require('../server');
+const {TEST_DATABASE_URL} = require('../config');
 
 chai.use(chaiHttp);
 
