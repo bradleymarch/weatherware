aea01cbc1fea95fc69023355f17f455e842037db
bradleymarch <bradleymarch@gmail.com>
2017-07-02 11:43:59 -0500

    _id

--- router.js
+++ router.js
@@ -162,7 +162,7 @@ router.get('/me', loggedIn, (req, res, next) => {
 );
 router.delete('/:id', (req, res) => {
   User
-  .findByIdAndUpdate(req.params.id)
+  .findByIdAndUpdate(req.params._id)
   .exec()
   .then(doc => {
     if (!doc) { return res.status(404).end(); }
