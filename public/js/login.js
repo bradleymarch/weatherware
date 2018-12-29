b2ee2419857d0d9f126c09f1422d0a51dd22d1bb
bradleymarch <bradleymarch@gmail.com>
2018-12-29 16:41:03 -0600

    more css

--- public/js/login.js
+++ public/js/login.js
@@ -29,7 +29,7 @@ function loginUser(username, password) {
         }
       else {
         $('.js-error-message').html('Could not load user');
-        
+
       };
 
     });
@@ -40,8 +40,6 @@ function loginUser(username, password) {
       $('.js-error-message').html('Invalid username or password');
     }
   });
-
-
 }
 
 function watchLogIn() {
@@ -55,49 +53,4 @@ function watchLogIn() {
   });
 }
 
-
-
 $(watchLogIn());
-
-
-/*'use strict';
-
-function loginUser(username, password) {
-
-  var settings = {
-    url: "/users/login",
-    method: "GET",
-    headers: {
-      'content-type': "application/json",
-      authorization: "basic" + btoa(username + ':' + password),
-    }, 
-  };
-//need to add localStorage - persist?
-
-  $.ajax(settings).done(function (response) {
-    console.log(response);
-      if (response.user) {
-        location.href = '/profile.html';
-        //now loggedIn, so MyWeatherWear anchor href now becomes href="profile.html";
-        $("#login_to_profile").attr("href", "/profile.html");
-      }
-      else {
-        $('.js-login-form')[0].reset();
-        $('.js-error-message').html('Invalid username or password');
-      }
-  });
-}
-
-function watchLogIn() {
-  $('.js-login-form').submit(function(event) {
-    event.preventDefault();
-    var username = $("#login_form_username").val();
-    var password = $("#login_form_password").val();
-    
-    loginUser(username, password);
-
-  })
-}
-
-$(watchLogIn());
-*/
